d8b1e8e4bcc4f892735e00f0f4b50d24e63bef3c
PittnoiR <omargarza394@gmail.com>
2024-10-03 22:29:58 -0600

    card con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      boxShadow:{
+        'custom-sombra':'10px 10px 0 black'
+      },
+      backgroundImage:{
+        'custom-img': 'url(/assets/images/illustration-article.svg)'
+      },
+      fontFamily:{
+        "custom-family": '[Figtree, system-ui]'
+      }
+    },
+  },
+  plugins: [], 
+}
+
